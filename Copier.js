ec2c161ad5deda6720af395d7ecbc47ce8bf6741
YoussofH <youssofhammoud@gmail.com>
2021-07-20 03:50:21 +0300

    Update Copier.js
    Copy stack overflow code snippets by overfloating button.

--- Copier.js
+++ Copier.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Copier
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  try to take over the world!
 // @author       You
 // @match        https://stackoverflow.com/*
